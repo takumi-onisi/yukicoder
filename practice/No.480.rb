6e99c94e8ccb7f6a59b00d3407ea2c1e2a0cf76c
takumi <f18416ks@gmail.com>
2020-09-13 11:26:33 +0900

    Solve new quizzes

---
+++ practice/No.480.rb
@@ -0,0 +1,9 @@
+N = gets.to_i
+# N = 3
+ans = 0
+
+(1..N).each do | index | 
+    ans += index
+end
+
+puts ans
---
+++ practice/No.9001.rb
@@ -0,0 +1,8 @@
+array_line1 = gets.split(nil)
+line2 = gets
+
+#テスト
+# array_line1 = "1 2".split(nil)
+# line2 = "yuki"
+
+puts "#{array_line1[0].to_i + array_line1[1].to_i} #{line2}"
