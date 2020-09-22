87a74e5c7b6c5239a63d26e38a1fadd8a6ed4601
takumi <f18416ks@gmail.com>
2020-09-22 14:23:07 +0900

    対数を利用した解法

--- practice/No.47.rb
+++ practice/No.47.rb
@@ -1,8 +1,7 @@
+#与えられた数Nに対して
+# 2**(x-1) < N <= 2**x
+# となるxを求める
+# 例 N=3の時　2**1<3<=2**2 であるからx=2
+# 1<log[2]3<=2
 N = gets.to_i
-
-(0..).each do | index | 
-    if (2**index >= N)
-        puts index
-        return
-    end    
-end
+puts Math.log2(N).ceil
