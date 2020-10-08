2c80e485a6b8e52b2415e650010c5b181e618953
takumi <f18416ks@gmail.com>
2020-10-08 15:12:12 +0900

    問題の解説を見ての回答

--- practice/No.46firstNSteps.rb
+++ practice/No.46firstNSteps.rb
@@ -6,6 +6,16 @@
 
 
 length_of_stride, distance = gets.split(nil).map(&:to_i)
-# 商は小数点以下の数字も欲しいため、割る数に1.0をかける
-needed_steps = (distance/(length_of_stride * 1.0)).ceil
+# aとbが整数の時 (a+(b-1))/b で商の少数以下を切り上げできる。
+needed_steps = ( distance + ( length_of_stride - 1 ) ) / length_of_stride
 puts needed_steps 
+
+# 個人的な考察（間違っている可能性あり）：
+# なぜaとbが整数の時 (a+(b-1))/b で商の少数以下を切り上げできるのか。
+#     整数同士の割り算において、
+#     ・割り切れる場合
+#     　　割られる数に(割る数-1)を足しても商に変化なし
+#     ・割り切れない時
+#     　　余りは必ず1よりも大きくかつ割る数よりも小さくなる
+#     　　従って割られる数に(割る数-1)の数を足すと、商が必ず+1される。（商が+2されることはない）
+
