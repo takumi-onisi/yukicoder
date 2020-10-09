e52c90301204db202e4ffc8c9f370fc08b27332d
takumi <f18416ks@gmail.com>
2020-10-09 17:53:11 +0900

    解答追加

---
+++ practice/No.264RockPaperScissors.rb
@@ -0,0 +1,32 @@
+# 問題文
+# https://yukicoder.me/problems/699
+
+# 自分と相手がじゃんけんをする。
+# じゃんけんの結果を標準出力に出力してください。
+# 結果は、自分が勝ったら「Won」、自分が負けたら「Lost」、引き分けなら「Drew」を出力してください。
+
+# 入力
+# N K
+# ぐー, ちょき, ぱーをそれぞれ 0, 1, 2とし、
+# １つ目に自分のが２つ目に相手のが与えられます。
+
+# 手の組み合わせ
+# 3×3の9通り
+# あいこ
+#     N-K=0
+# 勝つ場合
+#     (N,K) (0,1) (1,2) (2,0)
+# 負ける時
+#     それ以外
+
+hand_me, hand_opponent = gets.split(nil).map(&:to_i)
+if hand_me - hand_opponent == 0 then
+    puts "Drew" 
+
+elsif  hand_me == 0 && hand_opponent == 1 || hand_me == 1 && hand_opponent == 2 || hand_me == 2 && hand_opponent == 0  then
+    puts "Won"
+
+else    
+    puts "Lost"
+
+end
