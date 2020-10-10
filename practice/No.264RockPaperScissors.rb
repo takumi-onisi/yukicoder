e75f802249d00723c3c70244598224e370f139e5
takumi <f18416ks@gmail.com>
2020-10-10 11:11:34 +0900

    開設を見ての回答の修正

--- practice/No.264RockPaperScissors.rb
+++ practice/No.264RockPaperScissors.rb
@@ -10,20 +10,20 @@
 # ぐー, ちょき, ぱーをそれぞれ 0, 1, 2とし、
 # １つ目に自分のが２つ目に相手のが与えられます。
 
-# 手の組み合わせ
-# 3×3の9通り
 # あいこ
-#     N-K=0
+#     N == K
 # 勝つ場合
 #     (N,K) (0,1) (1,2) (2,0)
+#     上記の場合いずれも(N-K+3)%3 == 2となる
 # 負ける時
 #     それ以外
+#     (N-K+3)%3 == 1となる
 
 hand_me, hand_opponent = gets.split(nil).map(&:to_i)
-if hand_me - hand_opponent == 0 then
+if hand_me == hand_opponent then
     puts "Drew" 
 
-elsif  hand_me == 0 && hand_opponent == 1 || hand_me == 1 && hand_opponent == 2 || hand_me == 2 && hand_opponent == 0  then
+elsif (hand_me - hand_opponent + 3) % 3 == 2 then
     puts "Won"
 
 else    
