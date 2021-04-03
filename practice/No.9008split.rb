e9870c36b71605b7f1bdc3022bd82a859b0e5004
takumi <f18416ks@gmail.com>
2021-04-03 19:35:59 +0900

    他の人の回答を参考

--- practice/No.9008split.rb
+++ practice/No.9008split.rb
@@ -1,5 +1,9 @@
-n = gets
-nums = gets
-amount = 0
-nums.split(/\s+/){|num| amount += num.to_i }
-p amount
+# n = gets
+# nums = gets
+# amount = 0
+# nums.split(/\s+/){|num| amount += num.to_i }
+# p amount
+
+他の人の回答
+gets
+puts gets.split.map(&:to_i).sum 
