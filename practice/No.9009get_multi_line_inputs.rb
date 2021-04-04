de55eae9fc81aca5306c6216d50f3d7a27c3292b
takumi <f18416ks@gmail.com>
2021-04-04 13:10:31 +0900

    他の人の回答をメモ

--- practice/No.9009get_multi_line_inputs.rb
+++ practice/No.9009get_multi_line_inputs.rb
@@ -1,3 +1,7 @@
-amount = 0
-gets.to_i.times{ amount += gets.to_i }
-p amount 
+# amount = 0
+# gets.to_i.times{ amount += gets.to_i }
+# p amount 
+
+# 他の人の回答をメモ
+N = gets.to_i
+puts N.times.map{ gets.to_i }.sum
