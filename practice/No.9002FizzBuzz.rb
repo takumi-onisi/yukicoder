91febae690cfde498d2886c243a1eedf7763878c
takumi <f18416ks@gmail.com>
2021-04-07 19:06:57 +0900

    他の人の回答をメモ

--- practice/No.9002FizzBuzz.rb
+++ practice/No.9002FizzBuzz.rb
@@ -1,18 +1,21 @@
-(1..gets.to_i).each do |num|
-    if (num % 3 == 0) and not (num % 5 == 0) then
-      puts "Fizz"
-      next
-    end
+# (1..gets.to_i).each do |num|
+#     if (num % 3 == 0) and not (num % 5 == 0) then
+#       puts "Fizz"
+#       next
+#     end
     
-    if not (num % 3 == 0) and (num % 5 == 0) then
-      puts "Buzz"
-      next
-    end
+#     if not (num % 3 == 0) and (num % 5 == 0) then
+#       puts "Buzz"
+#       next
+#     end
     
-    if  (num % 3 == 0) and (num % 5 == 0) then
-      puts "FizzBuzz"
-      next
-    end
+#     if  (num % 3 == 0) and (num % 5 == 0) then
+#       puts "FizzBuzz"
+#       next
+#     end
     
-    puts num
-end
+#     puts num
+# end
+
+# 他の人の回答をメモ
+1.upto(gets.to_i){|n|puts'FizzBuzz '[i=n**4%-15,i+13]||n}
